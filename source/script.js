68d580cfae399a87dabfcc9fd3e6e0293c89b701
Amir_sh <mrshfkhn@gmail.com>
2022-05-29 18:46:06 +0430

    last change

--- source/script.js
+++ source/script.js
@@ -1,32 +1,25 @@
-let usernameElem = document.querySelector(".username");
-let passwordElem = document.querySelector(".password");
+let usernameInput = document.querySelector(".username");
+let passwordInput = document.querySelector(".password");
 let logginButtonElem = document.querySelector(".btn");
 let verificationMessage = document.querySelector(".verification-message");
 
 function verification() {
-  if (usernameElem.value.length <= 12) {
+  if (usernameInput.value.length <= 12 || passwordInput.value.length <= 8) {
     verificationMessage.style.display = "block";
+    verificationMessage.style.backgroundColor = "rgb(167 , 65 , 55)"
     verificationMessage.innerHTML =
-      "Username should be at least 12 Characters  :(";
+      "Username should be at least 12 and Password should be at least 8 characters :(";
     //   to hide the message
-    var timer = setInterval(function () {
+    setTimeout(function () {
       verificationMessage.style.display = "none";
-      clearInterval(timer);
-    }, 4000);
-  } else if (passwordElem.value.length <= 8) {
-    verificationMessage.style.display = "block";
-    verificationMessage.innerHTML =
-      "password should be at least 8 chracters  :(";
-    var timer = setInterval(function () {
-      verificationMessage.style.display = "none";
-      clearInterval(timer);
-    }, 4000);
+    },5000);
   } else {
     verificationMessage.style.display = "block";
+    verificationMessage.style.backgroundColor = "#00adb5"
     verificationMessage.innerHTML = "Succeessfuly logged in  :)";
-    var timer = setInterval(function () {
+    //   to hide the message
+    setTimeout(function () {
       verificationMessage.style.display = "none";
-      clearInterval(timer);
-    }, 4000);
+    }, 5000);
   }
 }
