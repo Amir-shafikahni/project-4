c147a06289955d5bf959a835f117c7c6cbd9f386
Amir_sh <mrshfkhn@gmail.com>
2022-05-29 18:49:07 +0430

    save

--- source/script.js
+++ source/script.js
@@ -6,16 +6,16 @@ let verificationMessage = document.querySelector(".verification-message");
 function verification() {
   if (usernameInput.value.length <= 12 || passwordInput.value.length <= 8) {
     verificationMessage.style.display = "block";
-    verificationMessage.style.backgroundColor = "rgb(167 , 65 , 55)"
+    verificationMessage.style.backgroundColor = "rgb(167 , 65 , 55)";
     verificationMessage.innerHTML =
       "Username should be at least 12 and Password should be at least 8 characters :(";
     //   to hide the message
     setTimeout(function () {
       verificationMessage.style.display = "none";
-    },5000);
+    }, 5000);
   } else {
     verificationMessage.style.display = "block";
-    verificationMessage.style.backgroundColor = "#00adb5"
+    verificationMessage.style.backgroundColor = "#00adb5";
     verificationMessage.innerHTML = "Succeessfuly logged in  :)";
     //   to hide the message
     setTimeout(function () {
